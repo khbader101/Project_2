89768306ae98bd00b964f564c6cf2e5ecccc44f3
khbader101 <69255197+khbader101@users.noreply.github.com>
2021-02-04 14:11:06 -0500

    set up jsonified data now need to make graphs with the data

--- static/app.py
+++ static/app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, MetaData
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 engine = create_engine("postgres://postgres:#Drummer101@localhost:5432/Video_Game_DB")
@@ -24,11 +24,11 @@ def welcome():
 
 @app.route("/game")
 def game():
-    data = engine.execute("SELECT * FROM video_game_view")
+    data = engine.execute("SELECT * FROM video_game_view2")
 
     game_list = []
 
-    for developer, number_of_games, avg_user_rating, total_number_of_raters, avg_price, primary_genre in data:
+    for developer, number_of_games, avg_user_rating, total_number_of_raters, avg_price, primary_genre, price_point_app in data:
         game_data = {}
         game_data["developer"] = developer
         game_data["number_of_games"] = float(number_of_games)
@@ -36,10 +36,33 @@ def game():
         game_data["total_number_of_raters"] = int(total_number_of_raters)
         game_data["avg_price"] = float(avg_price)
         game_data["primary_genre"] = primary_genre
+        game_data["price_point_app"] = price_point_app
 
         game_list.append(game_data)
         
     return render_template('index.html', game_list=game_list)
+
+@app.route("/data")
+def data():
+    tf = request.args.get("tf", 0)
+    data = engine.execute(f"SELECT * FROM video_game_view2 where price_point_app = {tf}")
+
+    game_list = []
+
+    for developer, number_of_games, avg_user_rating, total_number_of_raters, avg_price, primary_genre, price_point_app in data:
+        game_data = {}
+        game_data["developer"] = developer
+        game_data["number_of_games"] = float(number_of_games)
+        game_data["avg_user_rating"] = float(avg_user_rating)
+        game_data["total_number_of_raters"] = int(total_number_of_raters)
+        game_data["avg_price"] = float(avg_price)
+        game_data["primary_genre"] = primary_genre
+        game_data["price_point_app"] = price_point_app
+
+        game_list.append(game_data)
+
+    return jsonify(game_list)
+
 
 
 if __name__ == "__main__":
--- static/static/javascript/graphs.js
+++ static/static/javascript/graphs.js
@@ -1,13 +1,38 @@
-$.ajax({
-    url: "/game",
-    success: function (data) {
-        var fixed = $("#selDataset");
-        for (var i=0; i<data.length; i++) {
-            console.log(`${data[i].primary_genre}`);
-            fixed.append('option').text(data[i].primary_genre);
-            if (i<10) break;
+
+
+
+$("select").change(function () {
+    var tf = $("#selDataset option:selected").text();
+    $.getJSON($SCRIPT_ROOT + "/data", {
+        tf: $("#selDataset option:selected").text()
+
+    }, function (data) {
+        //chart functions
+        console.log(data.newinfo)
+    })
+
+});
+
+function buildplots(sampleData) {
+    d3.json('/data').then((dataset) => {
+
+        console.log(dataset)
+
+        var countTrace =
+        {
+            x: data["number_of_games"].slice(0, 10).reverse(), //count of games
+            y: data["developer"].slice(0, 10).map(value => `developer ${value}`).reverse(), //developers
+            type: "bar",
+            text: data["developer"].slice(0, 10).reverse(), //count of games?developers?
+            orientation: "h"
         };
-        //Object.entries(string).forEach(([i, j]) => {
-            //fixed.append('option').text(j);
-        //});
-}});
+        var countData = [countTrace];
+        var countLayout =
+        {
+            title: "Count of Games by Developer", //Count of Games by Developer
+            xaxis: { title: "# of Games" }, //# of Games
+        };
+        Plotly.newPlot("bar", countData, countLayout);
+
+    });
+};
